0dfdd0990cbfcc41cd62aebd05535cba696f8406
dougsko <dougtko@gmail.com>
2010-12-28 20:01:52 -0500

    works but needs more tests

--- spec/hotp_spec.rb
+++ spec/hotp_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Hotp" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+    it "pads count" do
+        hotp = Hotp.new("12345678901234567890", 0)
+        hotp.calculate.should == 755224
+    end
 end
