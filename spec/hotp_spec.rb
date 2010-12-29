5a48d9d722fa170ddc9fddb749f1bed7ba0fb8ab
Doug P <dougtko@gmail.com>
2010-12-28 21:43:24 -0500

    ...

--- spec/hotp_spec.rb
+++ spec/hotp_spec.rb
@@ -1,8 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Hotp" do
-    it "calculates correct OTP" do
-        hotp = Hotp.new("12345678901234567890", 0)
-        hotp.calculate.should == 755224
+    it "calculates HMAC SHA1 OTPs with length of 6" do
+        otps = [755224, 287082, 359152, 969429, 338314, 254676, 287922, 162583, 399871, 520489]
+        0.upto(9) do |i|
+            Hotp.new("12345678901234567890", i, 6, 'sha1').otp.should == otps[i]
+        end
     end
 end
