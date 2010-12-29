c64288208625794f7b069b352dbc321933997e3f
dougsko <dougtko@gmail.com>
2010-12-28 20:08:06 -0500

    edited test some

--- spec/hotp_spec.rb
+++ spec/hotp_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Hotp" do
-    it "pads count" do
+    it "calculates correct OTP" do
         hotp = Hotp.new("12345678901234567890", 0)
         hotp.calculate.should == 755224
     end
